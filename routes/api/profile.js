ac384383bd2365e51b4b3790837cc3888d32815d
Deepak Parihar <deepakparihar223@gmail.com>
2021-02-11 11:46:20 +0530

    Auth and sign up added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send("posts api working");
+    res.send("profile api working");
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const users = require('./routes/api/users');
+const auth = require('./routes/api/auth');
+const post = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
 
 const app = express();
+
+//Connect database
+connectDB();
+
+//Init middlewares
+app.use(express.json({
+    extented : false
+}))
 
 app.get('/', (req, res) => {
     res.send('API Working');
     // console.log("Fetched");
 })
+
+app.use('/api/users', users)
+app.use('/api/posts', post)
+app.use('/api/profile', profile)
+app.use('/api/auth', auth);
 
 
 const PORT = process.env.PORT || 3000;
